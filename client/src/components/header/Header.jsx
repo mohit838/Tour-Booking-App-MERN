01e3ee84fc92470e30f0cde9c654dfb6c6249f64
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-06-09 22:19:30 +0600

    Header search bar seetup
    header search bar with three input fields

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -6,6 +6,10 @@ import {
   faPlane,
   faCar,
   faTaxi,
+  faCalendar,
+  faCalendarDay,
+  faCalendarDays,
+  faPerson,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
@@ -13,7 +17,7 @@ const Header = () => {
     <div className="header">
       <div className="headerContainer">
         <div className="headerList">
-          <div className="headerListItems">
+          <div className="headerListItems active">
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
           </div>
@@ -32,6 +36,32 @@ const Header = () => {
           <div className="headerListItems">
             <FontAwesomeIcon icon={faTaxi} />
             <span>Airport Taxi</span>
+          </div>
+        </div>
+        {/* Header Title  */}
+        <h1 className="headerTitle">Find your next stay</h1>
+        <p className="headerDesc">
+          Search deals on hotels, homes, and much more...
+        </p>
+        <button className="headerButton">Signin / Register</button>
+
+        {/* Header Search Item Bar  */}
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} />
+            <input
+              type="text"
+              placeholder="Where you want to go?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} />
+            <span className="headerSearchText">Date To Date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} />
+            <span className="headerSearchText">2 adults 2 child 1 room</span>
           </div>
         </div>
       </div>
