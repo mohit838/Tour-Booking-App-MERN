1ce61f31fb1e57aecb93b9d80dc3ce1b5d9800f2
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-06-09 21:07:28 +0600

    Navbar Create
    Navbar Creating for projects

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/lists/List";
+import Hotel from "./pages/hotel/Hotel";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./home.css";
+import Navbar from "./../../components/navbar/Navbar";
+import Header from "./../../components/header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
