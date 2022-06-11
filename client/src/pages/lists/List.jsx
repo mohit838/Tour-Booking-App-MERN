c10a54281c6ff175b60aae21a896dd2117248320
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-06-11 13:52:03 +0600

    Editing Lists Setups
    Editing Lists

--- client/src/pages/lists/List.jsx
+++ client/src/pages/lists/List.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import "./list.css";
 
 const List = () => {
   return (
