7793de5bd99defa25e03050dd80b9c79496cbbc4
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-06-11 13:19:46 +0600

    Featured Propertise Complete

--- client/src/components/featuredProperties/featuredPropertise.jsx
+++ client/src/components/featuredProperties/featuredPropertise.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import "./featuredPropertise.css";
 
 const featuredPropertise = () => {
-  return <div></div>;
+  return (
+    <div className="fPropertise">
+      <div className="fPropertiseItems">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/178925192.jpg?k=448baf1f7ddc25eae7cff8afe1f1a44e85b100a43e1abbd97546bf4763d5005f&o=&hp=1"
+          alt=""
+          className="fPropertiseImg"
+        />
+        <spain className="fPName">Aprrta Hotel Martaso</spain>
+        <spain className="fPCity">Dublin</spain>
+        <spain className="fPPrice">Starting from $123</spain>
+        <div className="fPRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fPropertiseItems">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/178925192.jpg?k=448baf1f7ddc25eae7cff8afe1f1a44e85b100a43e1abbd97546bf4763d5005f&o=&hp=1"
+          alt=""
+          className="fPropertiseImg"
+        />
+        <spain className="fPName">Aprrta Hotel Martaso</spain>
+        <spain className="fPCity">Dublin</spain>
+        <spain className="fPPrice">Starting from $123</spain>
+        <div className="fPRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fPropertiseItems">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/178925192.jpg?k=448baf1f7ddc25eae7cff8afe1f1a44e85b100a43e1abbd97546bf4763d5005f&o=&hp=1"
+          alt=""
+          className="fPropertiseImg"
+        />
+        <spain className="fPName">Aprrta Hotel Martaso</spain>
+        <spain className="fPCity">Dublin</spain>
+        <spain className="fPPrice">Starting from $123</spain>
+        <div className="fPRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fPropertiseItems">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/178925192.jpg?k=448baf1f7ddc25eae7cff8afe1f1a44e85b100a43e1abbd97546bf4763d5005f&o=&hp=1"
+          alt=""
+          className="fPropertiseImg"
+        />
+        <spain className="fPName">Aprrta Hotel Martaso</spain>
+        <spain className="fPCity">Dublin</spain>
+        <spain className="fPPrice">Starting from $123</spain>
+        <div className="fPRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default featuredPropertise;
