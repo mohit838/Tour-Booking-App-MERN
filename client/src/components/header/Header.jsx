1ab8b726b2c5dc616f8ba1817396ebda6c19a8d5
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-06-11 19:50:10 +0600

    Update DateFNS
    Date Fns

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -115,7 +115,7 @@ const Header = ({ type }) => {
                     moveRangeOnFirstSelection={false}
                     ranges={date}
                     className="date"
-                    minDate={new date()}
+                    // minDate={new date()}
                   />
                 )}
               </div>
