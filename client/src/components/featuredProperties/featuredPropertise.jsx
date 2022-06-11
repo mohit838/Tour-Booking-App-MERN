6a29264672f763bb235c8ae9360aeb8eb7ea5303
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-06-11 12:54:10 +0600

    Featured Propertise Page Created

---
+++ client/src/components/featuredProperties/featuredPropertise.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const featuredPropertise = () => {
+  return <div></div>;
+};
+
+export default featuredPropertise;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import "./home.css";
 import Navbar from "./../../components/navbar/Navbar";
 import Header from "./../../components/header/Header";
 import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse By Properety Type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Guest Loves It!</h1>
       </div>
     </div>
   );
