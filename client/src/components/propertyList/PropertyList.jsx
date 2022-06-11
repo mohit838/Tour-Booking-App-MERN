1c69bdec13cf9f659e0dbde00a1d557109f1a3a2
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-06-11 12:53:42 +0600

    Propertise Page Finished

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import "./proppertyList.css";
 
 const PropertyList = () => {
-  return <div></div>;
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/364567017.jpg?k=60fbc38e0f97ce03106e04b910a7dbb71bf413798e88e85b50b2ace54d9db917&o=&hp=1"
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitle">
+          <h1>Hotels</h1>
+          <h2>123 Hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/364567017.jpg?k=60fbc38e0f97ce03106e04b910a7dbb71bf413798e88e85b50b2ace54d9db917&o=&hp=1"
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitle">
+          <h1>Hotels</h1>
+          <h2>123 Hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/364567017.jpg?k=60fbc38e0f97ce03106e04b910a7dbb71bf413798e88e85b50b2ace54d9db917&o=&hp=1"
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitle">
+          <h1>Hotels</h1>
+          <h2>123 Hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/364567017.jpg?k=60fbc38e0f97ce03106e04b910a7dbb71bf413798e88e85b50b2ace54d9db917&o=&hp=1"
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitle">
+          <h1>Hotels</h1>
+          <h2>123 Hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/364567017.jpg?k=60fbc38e0f97ce03106e04b910a7dbb71bf413798e88e85b50b2ace54d9db917&o=&hp=1"
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitle">
+          <h1>Hotels</h1>
+          <h2>123 Hotels</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PropertyList;
