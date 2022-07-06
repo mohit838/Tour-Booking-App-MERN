322d9b75368520879d39552e14e899411a1b143c
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-07-06 19:35:41 +0600

    Server site DB and Setups Routes

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    throw error;
+  }
+};
+
+//Middleware
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.get("/", (req, res) => {
+  res.send("This is Server!!");
+});
+
+app.listen(5000, () => {
+  connect();
+  console.log("Server Running!!");
+});
