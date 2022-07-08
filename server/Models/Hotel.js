54b4d148573dd0c65b5b11659c6fcabf75ed2179
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-07-08 19:35:17 +0600

    Hotel Update

--- server/Models/Hotel.js
+++ server/Models/Hotel.js
@@ -2,14 +2,17 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const hotelSchema = new Schema({
-  name: { type: Srting, required: true },
-  type: { type: Srting, required: true },
-  city: { type: Srting, required: true },
-  address: { type: Srting, required: true },
-  distance: { type: Srting, required: true },
-  image: { type: [Srting] },
-  desc: { type: Srting, required: true },
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  city: { type: String, required: true },
+  address: { type: String, required: true },
+  distance: { type: String, required: true },
+  image: { type: [String] },
+  desc: { type: String, required: true },
   rating: { type: Number, min: 0, max: 5 },
-  room: { type: [Srting] },
+  room: { type: [String] },
   cheapPrice: { type: Number, required: true },
+  feature: { type: Boolean, default: false },
 });
+
+export default mongoose.model("Hotel", hotelSchema);
--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,12 +1,26 @@
 import express from "express";
+import Hotel from "../Models/Hotel.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
+
+  try {
+    const saveHotel = await newHotel.save();
+    res.status(200).json(saveHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 //UPDATE
+
 //DELETE
+
 //GET
+
 //GET ALL
 
 export default router;
