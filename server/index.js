dfd0b73111a6f306a47075e08d4af199d9f8e78d
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-07-24 15:15:38 +0600

    Server Auth Creation

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -12,12 +13,18 @@ dotenv.config();
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
   } catch (error) {
     throw error;
   }
 };
 
-//Middleware
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+//middlewares
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -25,24 +32,18 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-//Handling Error
 app.use((err, req, res, next) => {
-  const errStatus = err.status || 500;
-  const errMsg = err.message || "Something went worng!";
-  return res.status(errStatus).json({
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
     success: false,
-    status: errStatus,
-    message: errMsg,
+    status: errorStatus,
+    message: errorMessage,
     stack: err.stack,
   });
 });
 
-//Test API
-app.get("/", (req, res) => {
-  res.send("This is Server!!");
-});
-
 app.listen(5000, () => {
   connect();
-  console.log("Server Running!!");
+  console.log("Connected to backend.");
 });
