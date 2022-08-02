dda3e587d4dd516c09f903515996ff732d72a8c5
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-02 15:25:42 +0600

    Server Password Setups

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
@@ -25,6 +26,7 @@ mongoose.connection.on("disconnected", () => {
 
 //middlewares
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
